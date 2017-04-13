ca58d80d7c254a87909037d0314262315635bae6
agnelnieves <agnelnieves@gmail.com>
2017-04-13 13:35:20 -0400

    Initial commit, initial layout setup

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//-------------------------
+//  Created by Agnel Nieves
+//  Feel free to contact me
+//  agnelnieves@gmail.com
+//  Please share :3
+//-------------------------
+
+
+// Additional options
+
+$(document).ready(function() {
+    $('select').material_select();
+    $('.parallax').parallax();
+    $('.modal').modal();
+    $('.scrollspy').scrollSpy();
+    $('.button-collapse').sideNav({
+    menuWidth: 300, 
+    edge: 'left', 
+    closeOnClick: true,
+    draggable: true 
+  });
+});
